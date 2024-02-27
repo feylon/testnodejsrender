09a5b87fcab914a1694fab1b19fc430e0945c59a
feylon <jamshid14092002@gmail.com>
2024-02-27 17:19:54 +0500

    yangilandi

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ import * as dotenv from "dotenv" ;
 dotenv.config();
 
 const app = express();
-
-let i = [1,3,4,5,6,6]
+app.use(cors());
+let i = [1,3,4,5,6,7,8]
 app.get("/",(req, res)=>{
     res.status(200).send(i);
 });
+
+
+app.post("/",(req,res)=>{
+    res.send({Salom:"Jamshid"})
+})
+
 console.log(process.env.PORT);
 const PORT = process.env.PORT;
 app.listen(PORT,()=>{
